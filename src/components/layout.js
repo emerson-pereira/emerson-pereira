e88a3685bf3095f4b93799556cad2b7c9f4b83d1
Emerson Pereira <emerson@emersonpereira.me>
2020-08-21 22:02:11 -0300

    make dark themed

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import { rhythm, scale } from "../utils/typography"
+
+const LayoutContainer = styled.div`
+  background: #333;
+  color: #f2f2f2;
+  height: 100vh;
+  padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
+
+  & > div {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: ${rhythm(24)};
+  }
+`
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -47,18 +61,13 @@ const Layout = ({ location, title, children }) => {
     )
   }
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <header>{header}</header>
-      <main>{children}</main>
-      <footer></footer>
-    </div>
+    <LayoutContainer>
+      <div>
+        <header>{header}</header>
+        <main>{children}</main>
+        <footer></footer>
+      </div>
+    </LayoutContainer>
   )
 }
 
