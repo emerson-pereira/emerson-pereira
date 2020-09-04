b5438ee7c85dad64a0c909611641488fb4f9ffe0
Emerson Pereira <emerson@emersonpereira.me>
2020-09-03 23:13:51 -0300

    implement rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,74 @@ module.exports = {
         trackingId: `UA-176568149-1`,
       },
     },
-    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                const siteUrl = site.siteMetadata.siteUrl
+                const postUrl = `${siteUrl}/blog${edge.node.fields.slug}`
+
+                let html = edge.node.html
+                html = html
+                  .replace(/href="\//g, `href="${siteUrl}/`)
+                  .replace(/src="\//g, `src="${siteUrl}/`)
+                  .replace(/"\/static\//g, `"${siteUrl}/static/`)
+                  .replace(/,\s*\/static\//g, `,${siteUrl}/static/`)
+
+                const footnote = `<div style="margin-top: 40px;"><p style="margin-top: 0; font-style: italic;">Originalmente publicado em <a href="${postUrl}">emersonpereira.me</a></p></div>`
+
+                return Object.assign({}, edge.node.frontmatter, {
+                  description:
+                    edge.node.frontmatter.description || edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: postUrl,
+                  guid: postUrl,
+                  custom_elements: [{ "content:encoded": html + footnote }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                        description
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Emerson Pereira's personal blog RSS Feed",
+            match: "^/blog/",
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
